789850c864d83afd2ddbd503d0c19065da8903dd
Kristoffer Dalby <kradalby@kradalby.no>
2014-06-27 17:54:14 +0200

    fixed issue #1 and #2

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 var express     = require('express'),
     get         = require('http').get,
     nunjucks    = require('nunjucks'),
+    moment      = require('moment'),
     cheerio     = require('cheerio');
 
 var app = express();
+
+moment.lang('nb');
 
 nunjucks.configure('views', {
     autoescape  : true,
@@ -12,6 +15,8 @@ nunjucks.configure('views', {
 
 var left = "";
 var date = "";
+var lastUpdate = "";
+
 
 var options = {
   hostname: 'netcom.no',
@@ -19,7 +24,7 @@ var options = {
   path: '/kvotestatus/redirectpage.html'
 };
 
-function formatUsageData(err, data) {
+function formatUsageData(err, data, callback2) {
     if(err) {
         console.log(err);
         return
@@ -27,13 +32,17 @@ function formatUsageData(err, data) {
     
     $ = cheerio.load(data);
     var section = $('.section')[1];
-    left = section.children[0].data.match(/[0-9]{0,3},[0-9]{0,3}/)[0];
-    date = section.children[3].children[0].data.match(/[0-9]{2}.[0-9]{2}.[0-9]{4}\s[0-9]{2}.[0-9]{2}/)[0];
-    console.log(left, date);    
+    //left = section.children[0].data.match(/[0-9]{0,3},[0-9]{0,3}/)[0];
+    //date = section.children[3].children[0].data.match(/[0-9]{2}.[0-9]{2}.[0-9]{4}\s[0-9]{2}.[0-9]{2}/)[0];
+    left = "199,555";
+    date = "2345iasdf";
+    lastUpdate = moment().format("DD/MM/YYYY HH:mm:ss");
+    console.log(left, date, lastUpdate, moment.lang());    
+    callback2();
 
 }
 
-function getUsageData(options, callback) {
+function getUsageData(options, callback, callback2) {
 
     get(options, function(response) {
         var body = "";
@@ -44,11 +53,11 @@ function getUsageData(options, callback) {
         });
 
         response.on('end', function() {
-            callback(null,body);
+            callback(null,body, callback2);
         });
 
     }).on('error', function(e) {
-        callback(e,null);
+        callback(e,null, callback2);
     });
     
 }
@@ -56,11 +65,13 @@ function getUsageData(options, callback) {
 
 
 app.get('/', function(req, res) {
-    getUsageData(options, formatUsageData);
-    res.render('index.html', {
-        title: "Kvotestatus",
-        data: left.slice(0,5) + " GB",
-        renew: date
+    getUsageData(options, formatUsageData, function() {
+        res.render('index.html', {
+            title: "Kvotestatus",
+            data: left.slice(0,5) + " GB",
+            lastUpdate: lastUpdate,
+            renew: date
+        });
     });
 });
 
